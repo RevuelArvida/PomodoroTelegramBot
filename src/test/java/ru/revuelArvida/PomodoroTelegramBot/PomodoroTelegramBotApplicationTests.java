6cffd88dc2fb0b1da28379745b6424a89e11d094
RevuelArvida <RevuelArvida15@gmail.com>
2021-02-19 12:12:19 +0300

    PTB-0: create Skeleton using Spring Boot

---
+++ src/test/java/ru/revuelArvida/PomodoroTelegramBot/PomodoroTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.revuelArvida.PomodoroTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
